7ae307415b82297c3a29605f46563851449b50d0
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-16 14:02:57 +0100

    feat: :sparkles: add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
 	import "../app.postcss";
+	let routes = ["login", "pizza", "multiplesteps", "tabsystem"];
 </script>
+
+<nav class="p-8">
+  {#each routes as route}
+	<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href={route}>{route}</a>
+  {/each}
+</nav>
 
 <slot />
 
