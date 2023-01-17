30accb08810fc37873833ea128b9066bd0aa1110
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-17 14:58:44 +0100

    fix: :zap: slight optimization

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,36 +1,45 @@
 <script>
 	let username = "";
 	let password = "";
-	let correct;
-	function submit() {
-		if (username === "admin" && password === "pass123") {
-			correct = "correct";
-		} else {
-			correct = "wrong";
-		}
-	}
+	let visible;
+	let wrong;
 </script>
 
-{#if correct === "correct"}
-	<div id="welcome">
-		<h1>Välkommen till 01 - Forms!</h1>
-	</div>
-{/if}
-<form id="form" on:submit={() => submit()}>
+<div id="welcome" class:visible class:hide={!visible}>
+	<h1>Välkommen till 01 - Forms!</h1>
+</div>
+<form
+	id="form"
+	on:submit={() => {
+		if (username === "admin" && password === "pass123") {
+			visible = true;
+			wrong = false;
+		} else {
+			visible = false;
+			wrong = true;
+		}
+	}}>
 	<label for="username">Username</label>
 	<input id="username" type="text" bind:value={username} />
 	<label for="password">Password</label>
 	<input id="password" type="password" bind:value={password} />
-	<button id="submit" type="submit" class="rounded-lg bg-blue-400">Submit</button>
+	<button id="submit" type="submit" class="columns-1 rounded-lg bg-blue-400">Submit</button>
 </form>
-{#if correct === "wrong"}
-	<div id="wrong">
-		<p>Användaruppgifterna är fel.</p>
-	</div>
-{/if}
+<div id="wrong" class={wrong ? "visible" : "hide"}>
+	<p>Användaruppgifterna är fel.</p>
+</div>
 
 <style>
 	* {
 		@apply p-4;
+	}
+	.visible {
+		display: block;
+	}
+	.hide {
+		display: none;
+	}
+	#wrong p {
+		color: red;
 	}
 </style>
