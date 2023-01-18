d41d174cca3ca690c184c3f0e09e2ef163f90de1
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-18 20:47:30 +0100

    fix: :bug: switch to static adapter for github pages support

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
-import adapter from "@sveltejs/adapter-auto";
-// import adapter from "@sveltejs/adapter-static";
+// import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 const dev = process.argv.includes("dev");
+// const dev = false;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			// base: dev ? "" : "/midiraku"
+			base: dev ? "" : "/frontend-projekt-23-svelte"
 		}
 	},
 	preprocess: [
