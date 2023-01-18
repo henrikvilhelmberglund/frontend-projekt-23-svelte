c73e40ca416672e83621a94b3ba2e40f85945226
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-18 20:08:23 +0100

    fix: :lipstick: style routes page to look more like the rest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
 </script>
 
-<div class="m-12 ">
-	<a class="rounded-lg bg-purple-500 p-4" href="/01">Lektion 1: Forms</a>
-	<a class="rounded-lg bg-purple-500 p-4" href="/02">Lektion 1: Validering</a>
+<div class="p-8 ">
+	<a class="rounded-lg p-4 m-4 bg-purple-500" href="/01">Lektion 1: Forms</a>
+	<a class="rounded-lg p-4 m-4 bg-purple-500" href="/02">Lektion 1: Validering</a>
 </div>
 
 <style>
