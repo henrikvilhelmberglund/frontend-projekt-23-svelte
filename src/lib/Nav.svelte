74a226661210d67f6ca703ed3f8718c6dbc76cc9
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-18 20:46:54 +0100

    fix: :bug: add base paths for certail links
    ? not all links? I hope it works

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -2,14 +2,14 @@
 	export let routes;
 	export let prefix;
 	import { page } from "$app/stores";
+  import { base } from "$app/paths";
 </script>
-
 <nav class="p-8">
-	<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href="/">Home</a>
-	<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href={prefix}>{prefix}</a>
+	<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href="{base}/">Home</a>
+	<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href={base}{prefix}>{prefix}</a>
 	{#each routes || [] as route}
 		{#if $page.route.id === prefix}
-			<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href="{prefix}/{route}">{route}</a>
+			<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href="{base}{prefix}/{route}">{route}</a>
 		{:else if $page.route.id.includes("/01/")}
 			<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href={route}>{route}</a>
 		{/if}
