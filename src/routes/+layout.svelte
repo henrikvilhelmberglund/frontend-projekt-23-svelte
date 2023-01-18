cdf13300f37da645d75bbbe448757d4ca7215b01
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-18 16:44:51 +0100

    fix: :bug: move +layout.svelte to subfolder for less ugly, use Nav component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,9 @@
 <script>
 	import "../app.postcss";
-	import { page } from "$app/stores";
-	let routes = ["login", "pizza", "multiplesteps", "tabsystem"];
+	
 </script>
 
-<nav class="p-8">
-	{#each routes as route}
-		{#if $page.route.id === "/01"}
-			<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href="01/{route}">{route}</a>
-		{:else if $page.route.id.includes("/01/")}
-			<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href={route}>{route}</a>
-		{/if}
-	{/each}
-</nav>
+
 
 <slot />
 
---
+++ src/routes/01/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Nav from "$lib/Nav.svelte";
+	let routes = ["login", "pizza", "multiplesteps", "tabsystem"];
+	let prefix = "/01";
+</script>
+
+<Nav {routes} {prefix} />
+<slot />
+
+<style>
+</style>
