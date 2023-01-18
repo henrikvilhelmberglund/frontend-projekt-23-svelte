f2738bcc84f8143c758a40c36a0a235d3a4f6d78
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-18 20:47:09 +0100

    fix: :bug: add base links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
+  import { base } from "$app/paths";
 </script>
 
 <div class="p-8 ">
-	<a class="rounded-lg p-4 m-4 bg-purple-500" href="/01">Lektion 1: Forms</a>
-	<a class="rounded-lg p-4 m-4 bg-purple-500" href="/02">Lektion 1: Validering</a>
+	<a class="rounded-lg p-4 m-4 bg-purple-500" href="{base}/01">Lektion 1: Forms</a>
+	<a class="rounded-lg p-4 m-4 bg-purple-500" href="{base}/02">Lektion 1: Validering</a>
 </div>
 
 <style>
