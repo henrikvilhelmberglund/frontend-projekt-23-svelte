d402b45c6e0355b9db084921cad2942c52ed3541
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-20 01:02:03 +0100

    fix: :bug: support JS routes (jank)

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,17 +1,32 @@
 <script>
 	export let routes;
 	export let prefix;
+	export let js;
 	import { page } from "$app/stores";
-  import { base } from "$app/paths";
+	import { base } from "$app/paths";
 </script>
+<!-- {$page.route.id} -->
+
 <nav class="p-8">
 	<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href="{base}/">Home</a>
-	<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href={base}{prefix}>{prefix}</a>
+	{#if !js}
+		<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href="{base}{prefix}">{prefix}</a>
+	{:else}
+		<a class="m-4 rounded-lg bg-indigo-400 p-4 text-black" href="{base}/js{prefix}">{prefix}</a>
+	{/if}
 	{#each routes || [] as route}
-		{#if $page.route.id === prefix}
+		{#if $page.route.id === prefix && !js}
 			<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href="{base}{prefix}/{route}">{route}</a>
-		{:else if $page.route.id.includes("/01/")}
+		{:else if $page.route.id.includes(prefix + "/") && !js}
 			<a class="m-4 rounded-lg bg-blue-400 p-4 text-black" href={route}>{route}</a>
+		{:else if $page.route.id === "/js" + prefix && js}
+			<a class="m-4 rounded-lg bg-indigo-400 p-4 text-black" href="{base}/js{prefix}/{route}"
+				>{route}</a>
+		{:else if $page.route.id.includes("/js" + prefix + "/") && js}
+			<a class="m-4 rounded-lg bg-indigo-400 p-4 text-black" href="{route}">{route}</a>
+      hi
+		{:else}
+			shit'sfuckedd
 		{/if}
 	{/each}
 </nav>
