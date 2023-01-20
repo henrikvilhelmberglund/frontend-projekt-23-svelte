d9c6b0de8f1af77e7721ea9c2260cdb648a15290
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-20 01:02:26 +0100

    feat: :sparkles: add link to JS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
-  import { base } from "$app/paths";
+	import { base } from "$app/paths";
 </script>
 
 <div class="p-8 ">
-	<a class="rounded-lg p-4 m-4 bg-purple-500" href="{base}/01">Lektion 1: Forms</a>
-	<a class="rounded-lg p-4 m-4 bg-purple-500" href="{base}/02">Lektion 1: Validering</a>
+	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/01">Lektion 1: Forms</a>
+	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/02">Lektion 1: Validering</a>
+	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js">JS</a>
 </div>
 
 <style>
