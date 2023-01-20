8432c3c562d3751eeaa4dd32cd51b88d9f535794
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-20 01:02:59 +0100

    feat: :sparkles: add nav for JS pages

---
+++ src/routes/js/01/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Nav from "$lib/Nav.svelte";
+	let routes = ["login", "pizza", "multiplesteps", "tabsystem"];
+	let prefix = "/01";
+	let js = true;
+</script>
+
+<Nav {js} {routes} {prefix} />
+<slot />
+
+<style>
+</style>
