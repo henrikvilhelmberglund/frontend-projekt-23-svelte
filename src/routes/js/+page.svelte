f3711cabde232f89a599408ee0ca562b7ecf7e64
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 09:53:16 +0100

    feat: :sparkles: add new link to 03scope

--- src/routes/js/+page.svelte
+++ src/routes/js/+page.svelte
@@ -5,6 +5,7 @@
 <div class="p-8 ">
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/01">Lektion 1: Forms</a>
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/02">Lektion 1: Validering</a>
+	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/03scope">Lektion 2: Scopes</a>
 </div>
 
 <style>
