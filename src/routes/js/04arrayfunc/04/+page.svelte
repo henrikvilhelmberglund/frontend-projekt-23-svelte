049f10b4dd45e4a8b0243c75cb0db4814c706333
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 11:33:01 +0100

    feat: :sparkles: add arrayfunc 04

---
+++ src/routes/js/04arrayfunc/04/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { onMount } from "svelte";
+	onMount(() => {
+		let body = document.querySelector("body");
+		body.style.display = "block";
+	});
+	const employees = [
+		{
+			firstname: "John",
+			lastname: "Doe",
+			role: "Fixer"
+		},
+		{
+			firstname: "Jane",
+			lastname: "Doe",
+			role: "Faxer"
+		},
+		{
+			firstname: "Robin",
+			lastname: "Hood",
+			role: "Fox"
+		}
+	];
+	let mappedEmployees = employees.map((person) => {
+		return person.firstname + " " + person.lastname;
+	});
+	console.log(mappedEmployees);
+	// Resultatet skall bli: ['John Doe', 'Jane Doe', 'Robin Hood']
+</script>
+
+<svelte:body bind:this={document} />
+
+<style>
+	:global(body) {
+		display: none;
+	}
+</style>
