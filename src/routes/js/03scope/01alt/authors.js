456b9bb88862644adcf4ff16c8dc8f477daad9a6
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-23 21:27:06 +0100

    refactor: :truck: move .js to 01alt

--- src/routes/js/03scope/01alt/authors.js
+++ src/routes/js/03scope/01alt/authors.js
@@ -2,7 +2,6 @@ var authors = ["Charles Dickens", "Jane Austen", "Mark Twain"];
 
 var btn = document.getElementById("showAuthorsBtn");
 var list = document.getElementById("authorList");
-
 btn.addEventListener("click", function () {
 	list.innerHTML = "";
 	for (var i = 0; i < authors.length; i++) {
