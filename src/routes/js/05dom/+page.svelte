1a13f1a5668a4c73454873f886b7ef2713ef5dfb
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 14:29:14 +0100

    feat: :sparkles: add 1a

---
+++ src/routes/js/05dom/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { onMount } from "svelte";
+	onMount(() => {
+		let body = document.querySelector("body");
+		body.style.display = "block";
+
+		let chatbox = document.createElement("aside");
+		chatbox.id = "chatbox";
+		let button = document.createElement("button");
+		button.innerText = "Show Chat";
+		button.addEventListener("click", (e) => {
+			chatbox.style.display = "block";
+		});
+		body.append(button);
+		let h2 = document.createElement("h2");
+		h2.innerText = "Chatbox";
+		chatbox.prepend(h2);
+		chatbox.style.display = "none";
+		body.append(chatbox);
+	});
+</script>
+
+<!-- <svelte:body bind:this={document} /> -->
+<style>
+	:global(body) {
+		display: none;
+	}
+</style>
