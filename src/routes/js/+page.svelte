fbb44dc0bcb19e3531c1d24bf6bf3e4c0a90e846
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 11:32:00 +0100

    feat: :sparkles: add nav

--- src/routes/js/+page.svelte
+++ src/routes/js/+page.svelte
@@ -6,6 +6,7 @@
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/01">Lektion 1: Forms</a>
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/02">Lektion 1: Validering</a>
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/03scope">Lektion 2: Scopes</a>
+	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/04arrayfunc">Lektion 2: Arrayfunc</a>
 </div>
 
 <style>
---
+++ src/routes/js/04arrayfunc/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Nav from "$lib/Nav.svelte";
+	let routes = ["01", "02", "03", "04", "05", "06"];
+	let prefix = "/04arrayfunc";
+	let js = true;
+</script>
+
+<Nav {js} {routes} {prefix} />
+<slot />
+
+<style>
+</style>
