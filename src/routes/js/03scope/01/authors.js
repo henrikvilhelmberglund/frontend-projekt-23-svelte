aa1a6b0c6cfd028e66e47221a95aa80504d539ff
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 10:05:36 +0100

    feat: :sparkles: add authors.js and quotes.js (unused for now)

---
+++ src/routes/js/03scope/01/authors.js
@@ -0,0 +1,13 @@
+var authors = ["Charles Dickens", "Jane Austen", "Mark Twain"];
+
+var btn = document.getElementById("showAuthorsBtn");
+var list = document.getElementById("authorList");
+
+btn.addEventListener("click", function () {
+	list.innerHTML = "";
+	for (var i = 0; i < authors.length; i++) {
+		var li = document.createElement("li");
+		li.innerText = authors[i];
+		list.appendChild(li);
+	}
+});
---
+++ src/routes/js/03scope/01/quotes.js
@@ -0,0 +1,17 @@
+var quotes = [
+	"The best is yet to be.",
+	"Try to be a rainbow in someones cloud.",
+	"When things go wrong, dont go with them."
+];
+
+var btn = document.getElementById("showQuotesBtn");
+var list = document.getElementById("quoteList");
+
+btn.addEventListener("click", function () {
+	list.innerHTML = "";
+	for (var i = 0; i < quotes.length; i++) {
+		var li = document.createElement("li");
+		li.innerText = quotes[i];
+		list.appendChild(li);
+	}
+});
