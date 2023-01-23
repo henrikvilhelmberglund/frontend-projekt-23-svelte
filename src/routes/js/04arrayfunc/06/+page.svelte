42b954124e002baddc78b285ed7e69ae6fbf188a
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 14:29:02 +0100

    feat: :sparkles: add janky antifilter() function

---
+++ src/routes/js/04arrayfunc/06/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { onMount } from "svelte";
+	onMount(() => {
+		let body = document.querySelector("body");
+		body.style.display = "block";
+	});
+
+	const numbers = [1, 2, 3, 4, 5];
+	let even = numbers.filter((num) => num % 2 === 0);
+
+	function antifilter(input, func) {
+		let found = input.filter((a) => {
+			return !eval(func);
+		});
+		return found;
+	}
+	console.log(numbers.filter((a) => a % 2 === 0));
+	console.log(antifilter(numbers, "a % 2 === 0"));
+</script>
+
+<svelte:body bind:this={document} />
+
+<style>
+	:global(body) {
+		display: none;
+	}
+</style>
