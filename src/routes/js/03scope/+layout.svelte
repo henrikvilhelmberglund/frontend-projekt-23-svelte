ab84a27f230085d44012e77b52c20b8809238176
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 09:54:08 +0100

    feat: :sparkles: add new Nav in 03scope

---
+++ src/routes/js/03scope/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Nav from "$lib/Nav.svelte";
+	let routes = ["01", "02", "03"];
+	let prefix = "/03scope";
+	let js = true;
+</script>
+
+<Nav {js} {routes} {prefix} />
+<slot />
+
+<style>
+</style>
