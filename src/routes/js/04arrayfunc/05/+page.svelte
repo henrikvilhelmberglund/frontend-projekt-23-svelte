35da453b61bf87270aada31fc4e57e686ac47058
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 11:33:14 +0100

    feat: :sparkles: add arrayfunc 05 (wip)

---
+++ src/routes/js/04arrayfunc/05/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { onMount } from "svelte";
+	onMount(() => {
+		let body = document.querySelector("body");
+		body.style.display = "block";
+	});
+	/*
+      Resultatet skall bli:
+      backgroundColor
+      listStyleImage
+      WebkitTransition (notera stort W)
+    */
+
+	const styles = ["background-color", "list-style-image", "-webkit-transition"];
+</script>
+
+<svelte:body bind:this={document} />
+
+<style>
+	:global(body) {
+		display: none;
+	}
+</style>
