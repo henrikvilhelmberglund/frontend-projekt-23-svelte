55bb11391abf8583a437bed406d1d27ef955dcaf
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 11:32:29 +0100

    feat: :sparkles: add array func 01

---
+++ src/routes/js/04arrayfunc/01/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { onMount } from "svelte";
+	onMount(() => {
+		let body = document.querySelector("body");
+		body.style.display = "block";
+
+		const validationResult = [
+			{
+				field: "name",
+				error: "Name is too short"
+			},
+			{
+				field: "description",
+				error: "Description is too long"
+			},
+			{
+				field: "price",
+				error: "Price is not a number"
+			},
+			{
+				field: "terms",
+				error: "Terms and conditions are not accepted"
+			}
+		];
+		let errors = document.querySelector("#errors");
+		validationResult.forEach((error) => {
+			errors.innerHTML += `${error.field}: ${error.error}<br>`;
+		});
+	});
+</script>
+
+<svelte:body bind:this={document} />
+
+<div id="errors" />
+
+<style>
+	:global(body) {
+		display: none;
+	}
+</style>
