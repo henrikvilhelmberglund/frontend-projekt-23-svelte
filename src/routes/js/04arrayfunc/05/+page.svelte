6a9ed605a8235424ec3ae452f1a0c31fb40385e1
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 13:05:49 +0100

    feat: :sparkles: add arrayfunc 05 (maybe stupid)

--- src/routes/js/04arrayfunc/05/+page.svelte
+++ src/routes/js/04arrayfunc/05/+page.svelte
@@ -10,8 +10,27 @@
       listStyleImage
       WebkitTransition (notera stort W)
     */
-
 	const styles = ["background-color", "list-style-image", "-webkit-transition"];
+	function toCamelCase(inputArray) {
+		let addCaps = 0;
+		let camelStyles = inputArray.map((style) => {
+			let editedStyle = style.split("").map((char) => {
+				if (char === "-") {
+					char = "";
+					addCaps = 1;
+				} else if (addCaps) {
+					char = char.toUpperCase();
+					addCaps = 0;
+				}
+				return char;
+			});
+			// console.log(editedStyle.join(""));
+			return editedStyle.join("");
+		});
+		return camelStyles;
+	}
+
+	console.log(toCamelCase(styles));
 </script>
 
 <svelte:body bind:this={document} />
