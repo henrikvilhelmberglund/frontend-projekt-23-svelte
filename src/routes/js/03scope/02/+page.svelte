49e512088dee6b1e1bb366cd7bc5f1cef8c527c6
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-01-23 10:08:53 +0100

    feat: :sparkles: add 02 in scope

---
+++ src/routes/js/03scope/02/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+	import { onMount } from "svelte";
+	onMount(() => {
+		let body = document.querySelector("body");
+		body.style.display = "block";
+
+		const messages = [
+			{
+				time: "10:12",
+				from: "bot",
+				text: "Välkommen till vår chat! Kan jag hjälpa till med något?"
+			},
+			{
+				time: "10:14",
+				from: "user",
+				text: "Något går fel när jag försöker bekräfta beställningen"
+			},
+			{
+				time: "10:20",
+				from: "bot",
+				text: 'Det verkar som du inte klickat i "godkänn"-rutan'
+			}
+		];
+
+		function getLastMessage(arr) {
+			arr.reverse();
+			return arr[0].text;
+		}
+
+		const latestMessageHolder = document.getElementById("latestMessage");
+		const showAllMessagesBtn = document.getElementById("showAllMessagesBtn");
+		const allMessagesList = document.getElementById("allMessages");
+
+		latestMessageHolder.innerText = getLastMessage(messages);
+
+		showAllMessagesBtn.addEventListener("click", function () {
+			messages.forEach(function (message) {
+				const li = document.createElement("li");
+				li.innerText = `${message.time}: ${message.text}`;
+				allMessagesList.appendChild(li);
+			});
+		});
+	});
+</script>
+
+<svelte:body bind:this={document} />
+
+<p id="latestMessage" />
+<button id="showAllMessagesBtn">Show message history</button>
+<ul id="allMessages" />
+
+<!-- <script type="text/javascript" src="main.js"></script> -->
+<style>
+	:global(body) {
+		display: none;
+	}
+</style>
