9e0ea7d0f93e6719e5286b1d7c98dbf53de75974
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-26 00:16:55 +0100

    feat: :sparkles: add 06async 04

---
+++ src/routes/js/06async/04/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { onMount } from "svelte";
+	let loaded = false;
+	onMount(() => {
+		let body = document.querySelector("body");
+		body.style.display = "block";
+		// import("./main.js");
+		delay(1000).then(function (result) {
+			console.log("We waited " + result + " ms"); // Körs efter 1000 millisekunder
+		});
+		delay(100).then(function (result) {
+			console.log("We waited " + result + " ms"); // Körs efter 1000 millisekunder
+		});
+		delay(5000).then(function (result) {
+			console.log("We waited " + result + " ms"); // Körs efter 1000 millisekunder
+		});
+	});
+	function delay(ms) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve(ms);
+			}, ms);
+		});
+	}
+</script>
+
+<!-- 
+  {#await wait}
+	<p>Waiting...</p>
+{:then value}
+	<p>We waited!</p>
+	<p>{value}</p>
+{/await} 
+-->
+
+<style>
+	:global(body) {
+		display: none;
+	}
+	#box {
+		position: absolute;
+		width: 200px;
+		height: 200px;
+		padding: 1em;
+		background-color: #e66767;
+		color: #fff;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+</style>
