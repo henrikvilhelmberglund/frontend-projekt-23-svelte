a0885208b551ee41b693c9b322f77abf95e44364
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-26 00:14:04 +0100

    feat: :sparkles: add 05 svelte version (a bit useless but hey)

---
+++ src/routes/05dom/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Nav from "$lib/Nav.svelte";
+	let routes = ["01"];
+	let prefix = "/05dom";
+	let js = true;
+</script>
+
+<Nav {routes} {prefix} />
+<slot />
---
+++ src/routes/05dom/01/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+	let hidden = false;
+	const messages = [
+		{
+			from: "bot",
+			text: "Välkommen till vår chat! Kan jag hjälpa till med något?"
+		},
+		{
+			from: "user",
+			text: "Något går fel när jag försöker bekräfta beställningen"
+		},
+		{
+			from: "bot",
+			text: 'Det verkar som du inte klickat i "godkänn"-rutan'
+		}
+	];
+</script>
+
+<h2>Chatbox</h2>
+<aside id="chatbox" class={hidden ? "" : "hidden"}>
+	<ul>
+		{#each messages as { from, text }}
+			<li class={from}>
+				{text}
+			</li>
+		{/each}
+	</ul>
+</aside>
+<button on:click={() => (hidden = !hidden)}>{hidden ? "Hide" : "Show"} chat</button>
+
+<style>
+	.hidden {
+		display: none;
+	}
+	.bot {
+		color: red;
+	}
+	.user {
+		color: blueviolet;
+	}
+</style>
