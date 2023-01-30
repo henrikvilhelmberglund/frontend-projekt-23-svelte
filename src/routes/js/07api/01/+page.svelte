19b8cf698e309e016f17e67385d3e3d2e80185b8
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-30 10:50:27 +0100

    feat: :sparkles: add mytext.txt in /src/lib and page that reads the file

---
+++ src/routes/js/07api/01/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { onMount } from "svelte";
+	let myP;
+
+	async function readTxt(address) {
+		const res = await fetch(address);
+		const data = await res.text();
+		myP.innerHTML = data;
+	}
+
+	onMount(() => {
+		let body = document.querySelector("body");
+		body.style.display = "block";
+		readTxt(`/src/lib/mytext.txt`);
+		// import("./main.js");
+	});
+</script>
+
+<p bind:this={myP} />
+
+<style>
+	:global(body) {
+		display: none;
+	}
+</style>
