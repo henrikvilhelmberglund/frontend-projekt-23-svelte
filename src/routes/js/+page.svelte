c4e66fdd1ff855ffcc3835ba4b883e370dd13233
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-30 10:49:19 +0100

    feat: :sparkles: add 07api link

--- src/routes/js/+page.svelte
+++ src/routes/js/+page.svelte
@@ -9,6 +9,7 @@
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/04arrayfunc">Lektion 2: Arrayfunc</a>
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/05dom">Lektion 2: DOM manipulation</a>
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/06async">Lektion 3: Async</a>
+	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js/07api">Lektion 4: API</a>
 </div>
 
 <style>
