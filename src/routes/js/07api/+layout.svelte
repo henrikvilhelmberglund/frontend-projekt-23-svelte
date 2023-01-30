ffe814e73afbbeabbe8b77fd7a355452d4d89abe
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-30 17:00:16 +0100

    fix: :fire: remove js prop

--- src/routes/js/07api/+layout.svelte
+++ src/routes/js/07api/+layout.svelte
@@ -2,10 +2,9 @@
 	import Nav from "$lib/Nav.svelte";
 	let routes = ["01", "02", "03", "04"];
 	let prefix = "/07api";
-	let js = true;
 </script>
 
-<Nav {js} {routes} {prefix} />
+<Nav {routes} {prefix} />
 <slot />
 
 <style>
