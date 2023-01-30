e1747ba09543323d65be160d9a639e2c81ed3f8f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-30 17:07:58 +0100

    feat: :sparkles: add link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,10 @@
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/01">Lektion 1: Forms</a>
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/02">Lektion 1: Validering</a>
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/03scope">Lektion 2: Scopes</a>
-	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/04arrayfunc">Lektion 2: Array Functions</a>
+	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/04arrayfunc"
+		>Lektion 2: Array Functions</a>
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/05dom">Lektion 2: DOM Manipulation</a>
+	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/07api">Lektion 4: API</a>
 	<a class="m-4 rounded-lg bg-purple-500 p-4" href="{base}/js">JS</a>
 </div>
 
