78bbc664543c59722c126f2c25390ae5b518475d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-30 17:14:09 +0100

    feat: :sparkles: add sveltekit version of 07api 01

--- src/routes/07api/01/+page.js
+++ src/routes/07api/01/+page.js
@@ -1,10 +1,8 @@
 /** @type {import('./$types').PageLoad} */
-export async function load() {
-	async function readTxt(address) {
-		const res = await fetch(address);
-		const text = await res.text();
-	}
+export async function load({ fetch, params }) {
+	const res = await fetch("/src/lib/mytext.txt");
+	const text = await res.text();
 	return {
-		text: await readTxt("/src/lib/mytext.txt")
+		text
 	};
 }
