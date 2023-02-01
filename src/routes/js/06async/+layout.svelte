fe5ac90d1ec5d7b9ff4e92c9bf5c2ad3b877aa4e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-01 20:40:58 +0100

    feat: :sparkles: add non SSR version of 06async 01
    you don't need onmount but is worse for SEO etc

--- src/routes/js/06async/+layout.svelte
+++ src/routes/js/06async/+layout.svelte
@@ -1,6 +1,19 @@
 <script>
 	import Nav from "$lib/Nav.svelte";
-	let routes = ["example", "example2", "example3", "01", "02", "03", "04", "05", "06", "07", "08"];
+	let routes = [
+		"example",
+		"example2",
+		"example3",
+		"01",
+		"01alt",
+		"02",
+		"03",
+		"04",
+		"05",
+		"06",
+		"07",
+		"08"
+	];
 	let prefix = "/06async";
 	let js = true;
 </script>
